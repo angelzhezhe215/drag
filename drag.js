291b2921724915ea3598ac314b39c6d74cdaef1b
zhouzhe <zhouzhe@tpfg.com.cn>
2018-01-04 14:21:47 +0800

    change drag.js

---
+++ drag.js
@@ -0,0 +1,9 @@
+function drag(id){
+	var obj = document.getElementById(id);
+	var disX = 0;
+	var disY = 0;
+	obj.onmousedown = function(ev){
+		disX = ev.pageX - obj.offsetLeft;
+		disY = ev.pageY - obj.offsetTop;
+	}
+}
