da3e92b054bed120cd4048df310a0fcc4855840f
zhouzhe <zhouzhe@tpfg.com.cn>
2018-01-04 15:32:25 +0800

    success drag

--- drag.js
+++ drag.js
@@ -9,5 +9,10 @@ function drag(id){
 			obj.style.left = ev.pageX - disX + 'px';
 			obj.style.top = ev.pageY - disY + 'px';
 		}
+		document.onmouseup = function(){
+			doucment.onmousemove = null;
+			document.onmouseup = null;
+		}
 	}
+	return false;
 }
