fefd4a453a2f90189a10a22519a5fa6eb46de4a1
zhouzhe <zhouzhe@tpfg.com.cn>
2018-01-04 14:39:32 +0800

    change drag.js from master

--- drag.js
+++ drag.js
@@ -5,5 +5,9 @@ function drag(id){
 	obj.onmousedown = function(ev){
 		disX = ev.pageX - obj.offsetLeft;
 		disY = ev.pageY - obj.offsetTop;
+		document.omousemove = function(ev){
+			obj.style.left = ev.pageX - disX + 'px';
+			obj.style.top = ev.pageY - disY + 'px';
+		}
 	}
 }
